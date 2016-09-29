03603722cf5dd20b4290d1721888328a1ae50974
nbkhope <ryumemaru@yahoo.com>
2016-09-29 11:12:58 -0700

    Rewrite first test (App)

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,20 @@
-import { renderComponent , expect } from '../test_helper';
+// Import helpers from test helper
+import { renderComponent, expect } from '../test_helper';
+
+// Component you want to test
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// describe: to group similar tests
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  // it: to test a single attribute of a target (one single test)
+  it('shows the correct text', () => {
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // expect: to make an assertion about a target
+    // ('target': the thing you're testing)
+    expect(component).to.contain('Testing a React-Redux App');
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
 });
