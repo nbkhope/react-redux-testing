134a3d0dbd0c7c5739b3275a986dcebb8a92ef27
nbkhope <ryumemaru@yahoo.com>
2016-09-29 11:48:39 -0700

    Add test to check for comment-box class and refactor component creation into beforeEach

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,17 +3,25 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  // declare variable that will change over time
+  let component;
+
+  // runs before each test (it)
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     // the above returns a jquery component, so allows you to
     // use jquery methods
     expect(component.find('textarea')).to.exist; // don't use () here
   });
+
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
-  it('', () => {
 
+  it('has the correct class', () => {
+    expect(component).to.have.class('comment-box');
   });
 });
