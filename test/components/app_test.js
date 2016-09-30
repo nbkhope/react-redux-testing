bb21e09cbe87325b6e07fbcb126fe05e7116b16f
nbkhope <ryumemaru@yahoo.com>
2016-09-30 12:22:22 -0700

    Update test for App to check presence of CommentList component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -24,4 +24,8 @@ describe('App', () => {
     expect(component.find('.comment-box')).to.exist;
   });
 
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
+  });
+
 });
