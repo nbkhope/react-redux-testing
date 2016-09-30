555d5f74924fd201bd4bad333dd8cf9a3aa97d44
nbkhope <ryumemaru@yahoo.com>
2016-09-30 11:49:15 -0700

    Change CommentBox div into a form and add event handlers for changing textarea and submitting form

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comment: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ comment: event.target.value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.setState({ comment: '' });
+  }
+
   render() {
     return (
-      <div className="comment-box">
-        <textarea />
-        <button type="submit">Add Comment</button>
-      </div>
+      <form className="comment-box" onSubmit={this.handleSubmit.bind(this)}>
+        <textarea
+          onChange={this.handleChange.bind(this)}
+          value={this.state.comment}
+        />
+        <button type="submit" className="btn btn-primary">Add Comment</button>
+      </form>
     );
   }
 }
